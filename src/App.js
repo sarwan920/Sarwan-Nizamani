86b96dcf8f726c28652215b8e90b0d94b7f32b42
Sarwan Nizamani <sarwan.nizamani8@gmail.com>
2021-10-27 23:58:06 +0500

    fix issue again

--- src/App.js
+++ src/App.js
@@ -6,21 +6,31 @@ import About from "./components/About";
 import Projects from "./components/Projects";
 import Contact from "./components/Contact";
 
+window.onscroll = function() {scrollFunction()};
+var mybutton = document.getElementById("myBtn");
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
 
 function App() {
 
 
   return (
     <div className="App">
+      <button  id="myBtn" title="Go to top">Top</button>
       <Header />
 
       <Introduction />
-{/* 
+
       <About />
 
       <Projects />
 
-      <Contact /> */}
+      <Contact />
     </div>
   );
 }
