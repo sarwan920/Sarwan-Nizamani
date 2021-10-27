f1884a61dbc318a9be869ed77c2b038033c61f69
Sarwan Nizamani <sarwan.nizamani8@gmail.com>
2021-10-28 03:38:20 +0500

    add animations

--- src/App.js
+++ src/App.js
@@ -6,23 +6,24 @@ import About from "./components/About";
 import Projects from "./components/Projects";
 import Contact from "./components/Contact";
 
-window.onscroll = function() {scrollFunction()};
-var mybutton = document.getElementById("myBtn");
-function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    mybutton.style.display = "block";
-  } else {
-    mybutton.style.display = "none";
-  }
-}
+import { Fade } from "react-reveal";
+// window.onscroll = function() {scrollFunction()};
+// var mybutton = document.getElementById("myBtn");
+// function scrollFunction() {
+//   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+//     mybutton.style.display = "block";
+//   } else {
+//     mybutton.style.display = "none";
+//   }
+// }
 
 function App() {
-
-
   return (
     <div className="App">
-      <button  id="myBtn" title="Go to top">Top</button>
-      <Header />
+      {/* <button  id="myBtn" title="Go to top">Top</button> */}
+      <Fade top>
+        <Header />
+      </Fade>
 
       <Introduction />
 
